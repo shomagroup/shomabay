8e518bd1e0200c90cd3ccdffe4ec1859b49343e8
shomagroup <kcasana@shomagroup.com>
2023-03-03 10:27:05 -0500

    Update home.js

--- home.js
+++ home.js
@@ -3,7 +3,7 @@ $('[sect-1-hover]').on('mouseenter mouseleave', function() {
     $(this).find('.sect-1-card-1, .sect-1-card-2').toggleClass('swaphover');
 });
 
-// SECTION 3'
+// SECTION 4
 $('.c33-2').on('mouseenter', function() {
     $('.c33-2').removeClass('hovered');
     $('.c33-2').removeClass('unhovered');
