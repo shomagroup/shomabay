e23f0e048d6a85f49e8ea0ba47b61bf86df9e4d7
shomagroup <kcasana@shomagroup.com>
2023-04-21 11:56:02 -0400

    Create CustomerResgistration.js

---
+++ CustomerResgistration.js
@@ -0,0 +1,17 @@
+$('.acknowledgement').on('click', function() {
+    $('[broker-form]').addClass('hidden');
+    if ($('.acknowledgement div.w--redirected-checked').length === 1) {
+        $('[broker-form]').removeClass('hidden');
+    }
+});
+
+$('.broker-button').on('click', function() {
+    $(this).toggleClass('clicked');
+    if ($('.broker-button').is('.clicked')) {
+        $('.broker-form-section').addClass('open');
+        $('.broker-form-section input[broker]').attr('required', '1');
+    } else {
+        $('.broker-form-section').removeClass('open');
+        $('.broker-form-section input[broker]').removeAttr('required');
+    }
+});
