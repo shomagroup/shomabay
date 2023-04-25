6df28921b059c84d93cfba81b0d6c26380e75fff
shomagroup <kcasana@shomagroup.com>
2023-04-25 11:42:00 -0400

    Update location.js

---
+++ location.js
@@ -0,0 +1,32 @@
+$('.nearby-link').on('click', function() {
+    $(this).toggleClass('active');
+
+    if ($('.nearby-link.active').length) {
+        $('.map-selector').addClass('hidden');
+    }
+
+    if ($('.nearby-link.active').is('[fine-dining]')) {
+        $("a[selector-type='fine-dining']").removeClass('hidden');
+    }
+    if ($('.nearby-link.active').is('[shopping]')) {
+        $("a[selector-type='shopping']").removeClass('hidden');
+    }
+    if ($('.nearby-link.active').is('[attractions]')) {
+        $("a[selector-type='attractions']").removeClass('hidden');
+    }
+    if ($('.nearby-link.active').is('[education]')) {
+        $("a[selector-type='education']").removeClass('hidden');
+    }
+    if (!$('.nearby-link.active').length) { $('.map-selector').removeClass('hidden'); }
+});
+
+$('.map-selector').on('click', function() {
+    $('.map-selector').removeClass('active');
+    if ($(this).is('.active')) {
+        $(this).removeClass('active');
+        $('.iframe-map iframe').attr('src', '');
+    } else {
+        $(this).addClass('active');
+        $('.iframe-map iframe').attr('src', $(this).attr('map') + '&zoom=-1');
+    }
+});
