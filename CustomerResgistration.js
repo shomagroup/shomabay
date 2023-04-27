bd41178a10d63c1ccff66efc2b25a912de8e3a0c
shomagroup <kcasana@shomagroup.com>
2023-04-27 13:27:30 -0400

    Update CustomerResgistration.js

--- CustomerResgistration.js
+++ CustomerResgistration.js
@@ -1,35 +1,35 @@
-// $('.acknowledgement').on('click', function() {
-//     $('[broker-form]').addClass('hidden');
-//     if ($('.acknowledgement div.w--redirected-checked').length === 1) {
-//         $('[broker-form]').removeClass('hidden');
-//     }
-// });
+$('.acknowledgement').on('click', function() {
+    $('[broker-form]').addClass('hidden');
+    if ($('.acknowledgement div.w--redirected-checked').length === 1) {
+        $('[broker-form]').removeClass('hidden');
+    }
+});
 
-// $('.broker-button').on('click', function() {
-//     $(this).toggleClass('clicked');
-//     if ($('.broker-button').is('.clicked')) {
-//         $('.broker-form-section').addClass('open');
-//         $('.broker-form-section input[broker]').attr('required', '1');
-//     } else if (!$('.broker-button').is('.clicked')) {
-//         $('.broker-form-section').removeClass('open');
-//         $('.broker-form-section input[broker]').removeAttr('required');
-//     }
-// });
+$('.broker-button').on('click', function() {
+    $(this).toggleClass('clicked');
+    if ($('.broker-button').is('.clicked')) {
+        $('.broker-form-section').addClass('open');
+        $('.broker-form-section input[broker]').attr('required', '1');
+    } else if (!$('.broker-button').is('.clicked')) {
+        $('.broker-form-section').removeClass('open');
+        $('.broker-form-section input[broker]').removeAttr('required');
+    }
+});
 
-// $('.acknowledgement').on('click', function() {
-//     $('[broker-form]').addClass('hidden');
-//     if ($('.acknowledgement div.w--redirected-checked').length === 1) {
-//         $('[broker-form]').removeClass('hidden');
-//     }
-// });
+$('.acknowledgement').on('click', function() {
+    $('[broker-form]').addClass('hidden');
+    if ($('.acknowledgement div.w--redirected-checked').length === 1) {
+        $('[broker-form]').removeClass('hidden');
+    }
+});
 
-// $('.broker-button').on('click', function() {
-//     $(this).toggleClass('clicked');
-//     if ($('.broker-button').is('.clicked')) {
-//         $('.broker-form-section').addClass('open');
-//         $('.broker-form-section input[broker]').attr('required', '1');
-//     } else if (!$('.broker-button').is('.clicked')) {
-//         $('.broker-form-section').removeClass('open');
-//         $('.broker-form-section input[broker]').removeAttr('required');
-//     }
-// });
+$('.broker-button').on('click', function() {
+    $(this).toggleClass('clicked');
+    if ($('.broker-button').is('.clicked')) {
+        $('.broker-form-section').addClass('open');
+        $('.broker-form-section input[broker]').attr('required', '1');
+    } else if (!$('.broker-button').is('.clicked')) {
+        $('.broker-form-section').removeClass('open');
+        $('.broker-form-section input[broker]').removeAttr('required');
+    }
+});
