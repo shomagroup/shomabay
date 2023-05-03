5cdc7b662130ab3854ff91d3f045b8c8fc8f306a
shomagroup <kcasana@shomagroup.com>
2023-05-03 09:21:31 -0400

    Update home.js

--- home.js
+++ home.js
@@ -1,3 +1,12 @@
+// OnReady 
+
+const phone = $('a.cta[phoneNum]').attr('href');
+if (url.includes('#phonering')) {
+    window.open(phone, "_top");
+}
+
+
+
 // SECTION 4
 $('.c33-2').on('mouseenter', function() {
     $('.c33-2').removeClass('hovered');
