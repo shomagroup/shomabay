696688a1bf1bd975414e847777cce20f95526c9e
shomagroup <kcasana@shomagroup.com>
2023-05-05 09:24:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // WEBSITE CODE VERSION
-console.log('ver 230505 0.2')
+console.log('ver 230505 0.3')
 
 // NAV CONFIG
 $('.nav-link').on('mouseenter', function() {
@@ -72,7 +72,7 @@ $('[agent]').on('click', function() {
 
 $('[name="country"]').on('click', function() {
     $(this).closest('form').find('[name="countryID"]').val($(this).find('option:selected').index());
-    if ($('[name="countryID"]').val() === 0 || $('[name="countryID"]').val() < 0) { $('[name="countryID"]').val('214'); }
+    if ($('[name="countryID"]').val() === "0" || $('[name="countryID"]').val() < "0") { $('[name="countryID"]').val('214'); }
 });
 
 // HIDDEN stuff
