d969cb7bc9954a6eea966989e3d14f279992aff6
shomagroup <kcasana@shomagroup.com>
2023-05-18 13:25:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // WEBSITE CODE VERSION
-console.log('ver 230518 0.1')
+console.log('ver 230518 0.2')
 
 // NAV CONFIG
 $('.nav-link').on('mouseenter', function() {
@@ -142,6 +142,48 @@ function time() {
 const timer = setInterval(time, 5000);
 
 
+
+// === UTM TRACKER === //
+$.urlParam = function(name) {
+    var results = new RegExp('[\?&]' + name + '=([^]*)').exec(window.location.href);
+    if (results == null) { return null; } else { return results[1] || 0; }
+}
+let cleanUrl = url.replace('https://www.shomabay.com/?', '').replace('https://shomabay.webflow.io/?', '');
+
+
+if (!$.urlParam('utm_source') == null || !$.urlParam('utm_source') == "") {
+    var source = $.urlParam('utm_source').split('&')[0].replace(/\+/g, ' ').replace(/%20/g, ' ');
+    Cookies.set('source', source, { expires: 30 });
+    Cookies.set('url', cleanUrl, { expires: 30 });
+}
+
+if (!$.urlParam('utm_medium') == null || !$.urlParam('utm_medium') == "") {
+    var medium = $.urlParam('utm_medium').split('&')[0].replace(/\+/g, ' ').replace(/%20/g, ' ');
+    Cookies.set('medium', medium, { expires: 30 });
+}
+
+if (!$.urlParam('utm_campaign') == null || !$.urlParam('utm_campaign') == "") {
+    var campaign = $.urlParam('utm_campaign').split('&')[0].replace(/\+/g, ' ').replace(/%20/g, ' ');
+    Cookies.set('campaign', campaign, { expires: 30 });
+}
+
+if (!$.urlParam('utm_term') == null || !$.urlParam('utm_term') == "") {
+    var term = $.urlParam('utm_term').split('&')[0].replace(/\+/g, ' ').replace(/%20/g, ' ');
+    Cookies.set('term', term, { expires: 30 });
+}
+
+$("input[name='current_url']").val(url);
+$("input[name='utm_url']").val(Cookies.get('url'));
+
+if (!Cookies.get('source') == null || !Cookies.get('source') == "") {
+    $("input[name='utm_source']").val(Cookies.get('source'));
+} else { $("input[name='utm_source']").val('Direct'); }
+
+$("input[name='utm_medium']").val(Cookies.get('medium'));
+$("input[name='utm_campaign']").val(Cookies.get('campaign'));
+$("input[name='utm_term']").val(Cookies.get('term'));
+
+
 //==== PHONE NUMBER FILTER ====//
 
 //---- GOOGLE
@@ -206,43 +248,3 @@ if ($.urlParam('utm_source') == 'google') {
     $('[phoneNum]').attr('href', 'tel:+17868825887');
     $('[phoneTx]').text('786-882-5887');
 }
-
-// === UTM TRACKER === //
-$.urlParam = function(name) {
-    var results = new RegExp('[\?&]' + name + '=([^]*)').exec(window.location.href);
-    if (results == null) { return null; } else { return results[1] || 0; }
-}
-let cleanUrl = url.replace('https://www.shomabay.com/?', '').replace('https://shomabay.webflow.io/?', '');
-
-
-if (!$.urlParam('utm_source') == null || !$.urlParam('utm_source') == "") {
-    var source = $.urlParam('utm_source').split('&')[0].replace(/\+/g, ' ').replace(/%20/g, ' ');
-    Cookies.set('source', source, { expires: 30 });
-    Cookies.set('url', cleanUrl, { expires: 30 });
-}
-
-if (!$.urlParam('utm_medium') == null || !$.urlParam('utm_medium') == "") {
-    var medium = $.urlParam('utm_medium').split('&')[0].replace(/\+/g, ' ').replace(/%20/g, ' ');
-    Cookies.set('medium', medium, { expires: 30 });
-}
-
-if (!$.urlParam('utm_campaign') == null || !$.urlParam('utm_campaign') == "") {
-    var campaign = $.urlParam('utm_campaign').split('&')[0].replace(/\+/g, ' ').replace(/%20/g, ' ');
-    Cookies.set('campaign', campaign, { expires: 30 });
-}
-
-if (!$.urlParam('utm_term') == null || !$.urlParam('utm_term') == "") {
-    var term = $.urlParam('utm_term').split('&')[0].replace(/\+/g, ' ').replace(/%20/g, ' ');
-    Cookies.set('term', term, { expires: 30 });
-}
-
-$("input[name='current_url']").val(url);
-$("input[name='utm_url']").val(Cookies.get('url'));
-
-if (!Cookies.get('source') == null || !Cookies.get('source') == "") {
-    $("input[name='utm_source']").val(Cookies.get('source'));
-} else { $("input[name='utm_source']").val('Direct'); }
-
-$("input[name='utm_medium']").val(Cookies.get('medium'));
-$("input[name='utm_campaign']").val(Cookies.get('campaign'));
-$("input[name='utm_term']").val(Cookies.get('term'));
