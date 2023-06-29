5c1c434d4e41dec778c148e8241bbc1f33b6ae87
shomagroup <92377989+shomagroup@users.noreply.github.com>
2023-06-29 15:13:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // WEBSITE CODE VERSION
-const codeVer = '230629 0.3';
+const codeVer = '230629 0.1';
 console.log(codeVer);
 $('div.codever').text(codeVer);
 
@@ -844,12 +844,14 @@ let cleanUrl = url.replace('https://www.shomabay.com/?', '').replace('https://sh
 
 
 
-var inTwoMins = new Date(new Date().getTime() + 2 * 60);
+var inTwoMins = 1/48;
+
+console.log(inTwoMins);
 
 if (!$.urlParam('utm_source') == null || !$.urlParam('utm_source') == "") {
     var source = $.urlParam('utm_source').split('&')[0].replace(/\+/g, ' ').replace(/%20/g, ' ');
     if (source == "presentation") {
-        Cookies.set('source', source, { expires: inTwoMins });
+        Cookies.set('source', source, { expires: 1/48 });
     } else {
     Cookies.set('source', source, { expires: 30 });
     Cookies.set('url', cleanUrl, { expires: 30 });
