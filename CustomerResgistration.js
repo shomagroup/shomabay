8d232582f12de607c075c9216f0c46be6b12cd01
shomagroup <92377989+shomagroup@users.noreply.github.com>
2023-06-29 13:08:50 -0400

    Update CustomerResgistration.js

--- CustomerResgistration.js
+++ CustomerResgistration.js
@@ -54,7 +54,10 @@ $('form[data-name="customer-registration"]').on('click touchstart', function() {
 // Cookies.set('campaign', 'ISG Brazilian Team', { expires: 1/5 });
 // Cookies.set('term', '05-1 05-2 23', { expires: 1/5 });
 
-$("input[name='utm_source']").val('Customer Registration');
+if (!Cookies.get('source') == null || !Cookies.get('source') == "") {
+    $("input[name='utm_source']").val(Cookies.get('source'));
+} else { $("input[name='utm_source']").val('Customer Registration'); }
+
 $("input[name='utm_medium']").val(Cookies.get('medium'));
 $("input[name='utm_campaign']").val(Cookies.get('campaign'));
 $("input[name='utm_term']").val(Cookies.get('term'));
