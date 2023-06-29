b800d60b5bf1aae55d0e0438c83a4e92c333196e
shomagroup <92377989+shomagroup@users.noreply.github.com>
2023-06-29 11:39:38 -0400

    Update CustomerResgistration.js

--- CustomerResgistration.js
+++ CustomerResgistration.js
@@ -1,3 +1,20 @@
+(function() {
+var cInfo = document.querySelector(".c-info");
+var cursorActive = function cursorActive(event) {
+cInfo.style.left = event.clientX + "px";
+cInfo.style.top = event.clientY + "px";
+};
+window.addEventListener("mousemove", cursorActive);
+})();
+
+$('[info]').on('mouseover mouseenter', function() {
+    $('.c-info').text($(this).attr('info'));
+    $('.c-info').addClass('active');
+});
+$('[info]').on('mouseout mouseleave', function() {
+    $('.c-info').removeClass('active');
+});
+
 $('.acknowledgement').on('click', function() {
     $('[broker-form]').addClass('hidden');
     if ($('.acknowledgement div.w--redirected-checked').length === 1) {
