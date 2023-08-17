05e55edc63fda1cb195eeb9bcce0571b9b5b7aa3
shomagroup <92377989+shomagroup@users.noreply.github.com>
2023-08-17 10:31:24 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // WEBSITE CODE VERSION
-const codeVer = '230817 0.0.0';
+const codeVer = '230816 0.0.0';
 console.log(codeVer);
 $('div.codever').text(codeVer);
 
@@ -953,38 +953,3 @@ if (Cookies.get('source') == 'google') {
 }
 
 //
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('#lang-swap').outerHeight();
-
-$(window).scroll(function(event){
-didScroll = true;
-});
-
-setInterval(function() {
-if (didScroll) {
-hasScrolled();
-didScroll = false;
-}
-}, 500);
-
-function hasScrolled() {
-var st = $(this).scrollTop();
-// Make sure they scroll more than delta
-if(Math.abs(lastScrollTop - st) <= delta)
-return;
-// If they scrolled down and are past the navbar, add class .nav-up.
-// This is necessary so you never see what is "behind" the navbar.
-if (st > lastScrollTop && st > navbarHeight){
-// Scroll Down
-$('#lang-swap').addClass('up');
-} else {
-// Scroll Up
-if(st + $(window).height() < $(document).height()) {
-$('#lang-swap').removeClass('up');
-}
-}
-lastScrollTop = st;
-}
