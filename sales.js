dda8b550a4e5d895c228222c34d71dabc0ce43f4
shomagroup <kcasana@shomagroup.com>
2024-04-11 10:29:16 -0400

    Update sales.js

--- sales.js
+++ sales.js
@@ -80,7 +80,6 @@ $('input[name="registration_type"]').on('change', function() {
     }
     });
     
-
     $('input[name="path_two_agent_type"]').on('change', function() {
     let $this = $(this).closest('label').attr('sub_path_selector');
     let options = ['new', 'reg'];
@@ -89,7 +88,7 @@ $('input[name="registration_type"]').on('change', function() {
     let unchosen = options.filter(option => option !== $this)[0];
 
     $(`input[req_agent]`).attr('readonly',true);
-    
+
     if (chosen === 'reg') {
     let def = '.form-wrapper[sub_path="reg"] .form-wrapper .form-wrapper:nth-child(2)';
     $(`${def} input[req_agent]`).attr('required',true).removeAttr('readonly');
@@ -130,6 +129,141 @@ $('input[name="registration_type"]').on('change', function() {
     $('input[name="message_send"]').val( user.signature + $(this).val() );
     }
     });
+
+
+// ---------- Lead Adder Start
+var leadcounter = 0; // Initialize counter
+
+function addlead() {
+if (leadcounter < 5) {
+leadcounter++
+
+let leadtoadd = $('<div>', { class: 'form-wrapper mod-vertical', 'data-count-id': leadcounter}).append(
+    $('<div>', { 
+        class: 'form-info-text mar-left-auto sub-title margin-mod', 
+        text: 'New Lead #'+leadcounter 
+    }),
+    $('<input>', {
+        type: 'text',
+        class: 'form-field w-input',
+        name: 'path_two_reg_lead_'+ leadcounter +'_fname',
+        'data-name': 'path_two_reg_lead_'+ leadcounter +'_fname',
+        id: 'path_two_reg_lead_'+ leadcounter +'_fname',
+        maxlength: '256',
+        required: 'required',
+        placeholder: 'First Name'
+    }),
+    $('<input>', {
+        type: 'text',
+        class: 'form-field w-input',
+        name: 'path_two_reg_lead_'+ leadcounter +'_lname',
+        'data-name': 'path_two_reg_lead_'+ leadcounter +'_lname',
+        id: 'path_two_reg_lead_'+ leadcounter +'_lname',
+        maxlength: '256',
+        required: 'required',
+        placeholder: 'Last Name'
+    }),
+    $('<div>', { class: 'form-wrapper mod-vertical' }).append(
+    $('<div>', { class: 'form-wrapper exception-horizontal',}).append(
+        $('<label>', { class: 'w-checkbox option s-form-check' }).append(
+            $('<div>', {
+                class:'w-checkbox-input w-checkbox-input--inputType-custom multi-option with-border mod-1 w--redirected-checked'
+            }),
+            $('<input>', {
+                type: 'checkbox',
+                class: 'form-field w-input',
+                name: 'path_two_reg_lead_'+ leadcounter +'_email_able',
+                style: 'opacity:0;position:absolute;z-index:-1',
+                'contact_option':'' , '_check':'' ,
+                'data-group':'reg_lead_'+ leadcounter,
+                required: 'required'
+            }),
+            $('<span>', {
+                class:'span-invi w-form-label',
+                for:'path_two_reg_lead_'+ leadcounter +'_email_able',
+                text:'✔'
+            })
+        ),
+        $('<input>', {
+            type:'text',
+            class: 'form-field w-input',
+            name: 'path_two_reg_lead_'+ leadcounter +'_email',
+            'data-name': 'path_two_reg_lead_'+ leadcounter +'_email',
+            id: 'path_two_reg_lead_'+ leadcounter +'_email',
+            maxlength: '256',
+            required: 'required',
+            placeholder: 'Email'
+        })
+    ),
+    $('<div>', { class: 'form-wrapper exception-horizontal',}).append(
+        $('<label>', { class: 'w-checkbox option s-form-check' }).append(
+            $('<div>', {
+                class:'w-checkbox-input w-checkbox-input--inputType-custom multi-option with-border mod-1 w--redirected-checked'
+            }),
+            $('<input>', {
+                type: 'checkbox',
+                class: 'form-field w-input',
+                name: 'path_two_reg_lead_'+ leadcounter +'_phone_able',
+                style: 'opacity:0;position:absolute;z-index:-1',
+                'contact_option':'' , '_check':'' ,
+                'data-group':'reg_lead_'+ leadcounter,
+                required: 'required'
+            }),
+            $('<span>', {
+                class:'span-invi w-form-label',
+                for:'path_two_reg_lead_'+ leadcounter +'_phone_able',
+                text:'✔'
+            })
+        ),
+        $('<input>', {
+            type:'text',
+            class: 'form-field w-input',
+            name: 'path_two_reg_lead_'+ leadcounter +'_phone',
+            'data-name': 'path_two_reg_lead_'+ leadcounter +'_phone',
+            id: 'path_two_reg_lead_'+ leadcounter +'_phone',
+            maxlength: '256',
+            required: 'required',
+            placeholder: 'Phone'
+        })
+    )
+    )
+);
+
+$('.form-wrapper[data-lead-container]').append(leadtoadd);
+
+if (leadcounter === 5) {
+$('.lead-add-remove[trigger-add-lead]').addClass('disabled');
+}
+if (leadcounter > 0) {
+$('.lead-add-remove[trigger-re-add-lead]').removeClass('disabled');
+}
+} else {alert("Maximum of 5 leads per submission")}}
+
+function addlead_remove() {
+$('.form-wrapper[data-count-id="'+leadcounter+'"]').remove();
+
+if (leadcounter > 0) {
+leadcounter--;
+}
+if (leadcounter === 0) {
+$('.lead-add-remove[trigger-re-add-lead]').addClass('disabled');
+}
+if (leadcounter < 5) {
+$('.lead-add-remove[trigger-add-lead]').removeClass('disabled');
+}}
+
+function addlead_reset() {
+$('.form-wrapper[data-lead-container]').empty();
+leadcounter = 0; // Reset counter
+}
+
+$('[trigger-add-lead]').on('click', function() { 
+addlead();
+});
+$('[trigger-re-add-lead]').on('click', function() { 
+addlead_remove();
+});
+// ---------- Lead Adder End
 
 
 }); // DOM LOADED
